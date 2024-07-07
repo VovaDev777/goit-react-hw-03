390987bef78a7e51d3c071c6730004e79d0b759f
VovaDev777 <polvovater@gmail.com>
2024-07-07 09:38:41 +0200

    1

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,6 +1,9 @@
 import ContactForm from "../ContactForm/ContactForm"
+import { useState } from "react"
 
 const App = () => {
+  [contacts, setContact] = useState();
+
   return (
     <>
       <h1>Phonebook</h1>
